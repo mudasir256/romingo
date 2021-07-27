de5aec46d1704cd784577775c97c8030035e8266
Chris Hiller <chris@hiller.io>
2021-07-27 06:29:47 -0700

    changed app to reflect new listing card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 /* eslint-disable */
 import React from "react";
 
-import { ListingCard } from "./components/ListingCard/ListingCard";
+import { ListingCard3 } from "./components/ListingCard/Example3/ListingCard3";
 const App = () => {
   return (
     <div className="App">
-      <ListingCard title="test" paragraph="testing1234" />
+      <ListingCard3
+        imageSrc="https://bit.ly/2EApSiC"
+        propertyName="Hilton Tucson"
+        location="6330 N Papaya Pl, Tucson AZ 85741"
+        currency="$"
+        price="100"
+        amenity1="Dog park nearby"
+        amenity2="Pet friendly patio"
+      />
     </div>
   );
 };
