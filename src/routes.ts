3e4dc51dbe6052f2f31bcb67a354957987ff0792
Chris Hiller <chris@hiller.io>
2021-08-12 14:50:59 -0700

    minor cleanup

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,24 @@
 import { FC } from "react";
 import HomePage from "./pages/HomePage";
+import ListingPage from "./pages/ListingPage";
 
 interface Routes {
-	path: string;
-	component: FC<any>;
-	requireAuth: boolean;
+  path: string;
+  component: FC<any>;
+  requireAuth: boolean;
 }
 
 const routes: Routes[] = [
-	{
-		path: "/",
-		component: HomePage,
-		requireAuth: false
-	},
-]
+  {
+    path: "/",
+    component: HomePage,
+    requireAuth: false,
+  },
+  {
+    path: "/listings",
+    component: ListingPage,
+    requireAuth: false,
+  },
+];
 
 export default routes;
--- src/store/searchReducer.ts
+++ src/store/searchReducer.ts
@@ -1,49 +1,52 @@
 import * as actionTypes from "./actionTypes";
 
-export function saveSearch(search: ISearch) {
-	const action: SearchAction = {
-		type: actionTypes.SAVE_SEARCH,
-		search
-	}
+export const saveSearch = (search: ISearch) => {
+  const action: SearchAction = {
+    type: actionTypes.SAVE_SEARCH,
+    search,
+  };
 
-	return simulateHttpRequest(action)
-}
+  return simulateHttpRequest(action);
+};
 
-export function removeArticle(search: ISearch) {
-	const action: SearchAction = {
-		type: actionTypes.REMOVE_SEARCH,
-		search,
-	}
-	return simulateHttpRequest(action)
-}
+export const removeArticle = (search: ISearch) => {
+  const action: SearchAction = {
+    type: actionTypes.REMOVE_SEARCH,
+    search,
+  };
+  return simulateHttpRequest(action);
+};
 
-export function simulateHttpRequest(action: SearchAction) {
-	return (dispatch: SearchDispatchType) => {
-		setTimeout(() => {
-			dispatch(action)
-		}, 500)
-	}
-}
+export const simulateHttpRequest = (action: SearchAction) => {
+  return (dispatch: SearchDispatchType) => {
+    setTimeout(() => {
+      dispatch(action);
+    }, 500);
+  };
+};
 
 const initialState: SearchState = {
-	search: {
-		city: "San Francisco",
-		checkIn: "8/1/2021",
-		checkOut: "8/11/2021",
-		occupants: {
-			adults: 1,
-			children: 1,
-			dogs: 1
-		}
-	}
-}
+  search: {
+    city: "San Francisco",
+    checkIn: "8/1/2021",
+    checkOut: "8/11/2021",
+    occupants: {
+      adults: 1,
+      children: 1,
+      dogs: 1,
+    },
+  },
+};
 
-const searchReducer = (state: SearchState = initialState, action: SearchAction): SearchState => {
-	switch (action.type) {
-		case actionTypes.SAVE_SEARCH:
-			return {search: action.search}
-	}
-	return state;
-}
+const searchReducer = (
+  state: SearchState = initialState,
+  action: SearchAction
+): SearchState => {
+  switch (action.type) {
+    case actionTypes.SAVE_SEARCH:
+      return { search: action.search };
+  }
+  return state;
+};
 
 export default searchReducer;
