70c3e985da2100514de36df5918e0cd17395cfe4
Will Gu <willgu29@gmail.com>
2022-10-27 22:34:56 -0400

    seo load blog posts

--- src/sitemap.js
+++ src/sitemap.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const csv = require('fast-csv');
+const fetch = require('node-fetch')
 
 const Sitemap = require("react-router-sitemap").default;
 
@@ -76,9 +77,24 @@ const routes = [
 ];
 
 const aliasMap = []
-
-//TODO: blog ids
 const idMap = []
+
+async function loadBlogPosts(page) {
+  const result = await fetch(`https://blog.romingo.com/wp-json/wp/v2/posts?page=${page}&per_page=50&_embed&_fields=id,excerpt,title,link,_links,_embedded`)
+  const json = await result.json()
+  const total = result.headers.get("x-wp-total")
+  for (let i = 0 ; i < json.length; i++) {
+    const postId = json[i].id
+    idMap.push({ id: postId })
+  }
+
+  if (total > (50 * page)) {
+    await loadBlogPosts(page + 1)
+  } else {
+    console.log('blog seo posts loaded')
+  }
+}
+
 
 fs.createReadStream('./hotels.csv')
   .pipe(csv.parse({ headers: false }))
@@ -88,7 +104,8 @@ fs.createReadStream('./hotels.csv')
       alias: row[22],
     })
   })
-  .on('end', () => {
+  .on('end', async () => {
+    await loadBlogPosts(1)
     const paramsConfig = {
       "/hotel/:alias": aliasMap,
       "/blog/post/:id": idMap
