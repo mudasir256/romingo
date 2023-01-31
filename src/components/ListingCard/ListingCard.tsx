fa79dc8d1491780dd110063658ebaee33bb090c6
Will Gu <willgu29@gmail.com>
2023-01-31 02:00:06 -0500

    patchfix listings

--- src/components/ListingCard/ListingCard.tsx
+++ src/components/ListingCard/ListingCard.tsx
@@ -219,8 +219,7 @@ const ListingCard: FC<ListingCardProps> = ({
             images={imageURLs}
             name={name}
             sx={{
-              width: { xs: "100%", sm: 250, md: 200, lg: 200 },
-              height: { xs: 250, sm: 212, md: 187 },
+
               borderTopRightRadius: { xs: "6px", sm: 0 },
               borderBottomRightRadius: { xs: 0, sm: 0 },
               borderBottomLeftRadius: { xs: 0, sm: "6px" },
