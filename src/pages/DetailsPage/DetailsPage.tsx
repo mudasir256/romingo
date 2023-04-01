175dfb17e0e57f921dcc9dd249a976abbfbedd0c
Will Gu <willgu29@gmail.com>
2023-04-01 11:33:16 -0400

    text

--- src/pages/DetailsPage/DetailsPage.tsx
+++ src/pages/DetailsPage/DetailsPage.tsx
@@ -1571,7 +1571,7 @@ const DetailsPage: FC<Props> = ({ ...props }) => {
             >
               Compare Rates
             </Typography>
-
+            <Typography variant="base">Book with Romingo.com to get the best rates at pet-friendly hotels. Romingo guests pay $0 pet fees and receive VIP pet amenities upon arrival.</Typography>
             <Box display="flex" gap="1rem" sx={{ flexDirection: { xs: 'column', sm: 'column', md: 'row'}  }}>
               <Box position="relative" py="1rem" px="1.5rem" textAlign="center" border="solid 1px black">
                 <div><img style={{width: '206px'}} src="https://romingo.com/static/media/logo.11150e63.png" /></div>
