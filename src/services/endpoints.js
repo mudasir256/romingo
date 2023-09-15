c713f95b36cbfaff3170a20d59494878deb9276f
Will Gu <willgu29@gmail.com>
2023-09-15 15:34:12 -0400

    add mailchimp

--- src/services/endpoints.js
+++ src/services/endpoints.js
@@ -2,6 +2,13 @@ export const subscribeToNewsletter = (email) => {
   if (!email) {
     return { success: false }
   }
+  fetch(process.env.REACT_APP_BASE_ENDPOINT + `v2/mailchimp/signup/${email}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
   fetch(
     `https://romingo.us6.list-manage.com/subscribe/post-json?u=585083137c3540a7371e3a74f&id=d4d3932414&EMAIL=${encodeURIComponent(
       email
